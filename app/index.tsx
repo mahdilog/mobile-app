b925cc90f8ad9eb82897d09d5cdf94e08bbdcb09
mahdi-mohamadi <mahdiprog81@gmail.com>
2024-04-26 23:58:20 +0330

    feat: implement dark and light mode

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,23 @@
 //@ts-nocheck
 import React from "react";
-import { RootContainer } from "@/components/defaultLayout";
-import { Text, View } from "react-native";
+import { RootContainer } from "@/components/layouts/defaultLayout";
+import { Landing } from "@/pages/landing";
+import { Pressable, Text } from "react-native";
+import { styled, useColorScheme } from "nativewind";
 
 export default function App() {
+  const { colorScheme, toggleColorScheme } = useColorScheme();
   return (
     <RootContainer>
-      <View></View>
+      <Pressable
+        onPress={toggleColorScheme}
+        className="flex-1 items-center justify-center"
+      >
+        <Text selectable={false} className="text-purple-500 dark:text-white">
+          {`Try clicking me! ${colorScheme === "dark" ? "🌙" : "🌞"}`}
+        </Text>
+      </Pressable>
+      <Landing />
     </RootContainer>
   );
 }
