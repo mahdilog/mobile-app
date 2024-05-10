a51f01516f8b125d8c0e6c29ae4d105f25e37672
ehsan <m.ehsan1381@gmail.com>
2024-05-10 15:38:07 +0330

    fix header layout

--- src/layouts/landing.tsx
+++ src/layouts/landing.tsx
@@ -8,30 +8,56 @@ export const LandingLayout = ({ children }: { children: ReactNode }) => {
   const router = useRouter();
   return (
     <>
-      <ScrollView marginBottom="12" bg="#f5f5f5">
-        <View p={8} bg="#D9DCD6" fontFamily="body" h="208px">
-          <Box justifyContent="space-between" flexDir="row" alignItems="center">
-            <View bg="white" p={3} borderRadius={999} shadow={1}>
-              <Text>
-                <Iconify
-                  icon="mdi:user"
-                  size={24}
-                  color="#2F6690"
-                  onTouchEnd={() => router.navigate("/login")}
-                />
-              </Text>
-            </View>
-            <View>
-              <Text fontFamily="heading">نوا</Text>
-            </View>
-            <View bg="white" p={3} borderRadius={999} shadow={1}>
-              <Text>
-                <Iconify icon="mingcute:search-3-line" size={24} color="#2F6690" />
-              </Text>
-            </View>
-          </Box>
+      <View
+        bg="#D9DCD6"
+        fontFamily="body"
+        h="100px"
+        justifyContent="space-between"
+        flexDir="row"
+        alignItems="center"
+        paddingX="20px"
+        paddingTop="25px"
+      >
+        <View
+          bg="white"
+          w="50px"
+          h="50px"
+          justifyContent="center"
+          alignItems="center"
+          borderRadius={999}
+          shadow={1}
+        >
+          <Text>
+            <Iconify
+              icon="mdi:user"
+              size={24}
+              color="#2F6690"
+              onTouchEnd={() => router.navigate("/login")}
+            />
+          </Text>
         </View>
-        <View bg="#f5f5f5">{children}</View>
+        <View>
+          <Text fontFamily="heading">نوا</Text>
+        </View>
+        <View
+          bg="white"
+          w="50px"
+          h="50px"
+          justifyContent="center"
+          alignItems="center"
+          borderRadius={999}
+          shadow={1}
+        >
+          <Text>
+            <Iconify icon="mingcute:search-3-line" size={24} color="#2F6690" />
+          </Text>
+        </View>
+      </View>
+      <ScrollView marginBottom="12" bg="#D9DCD6">
+        <View bg="#f5f5f5">
+          <View h={"130px"} backgroundColor="#D9DCD6" />
+          {children}
+        </View>
       </ScrollView>
       <View position="absolute" w="full" h="60px" bottom="0">
         <Footer />
